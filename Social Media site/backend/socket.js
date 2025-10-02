07ae6e3b3e203c36ca7dd58ddc9f411b4118ddec
AbhinavSharma0188 <abhinavsharma3614@gmail.com>
2025-10-02 10:52:20 +0530

    Update socket.js

--- Social Media site/backend/socket.js
+++ Social Media site/backend/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: "https://vybe-svus.onrender.com", // frontend URL
     methods: ["GET", "POST"],        // must be plural
   },
 });
